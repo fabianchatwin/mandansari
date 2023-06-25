24a9606f6050e76fbb307db5f00af1078506ef22
fabian chatwin cedrati <fabianchatwin@gmail.com>
2023-06-25 19:09:37 +0000

    first commit test

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Mandansari</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Next.js Mandansari Toolbox" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          Jimbo jambo Here's an example of a Netlify Form! When you fill this
+          out, the submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
         <JokeBlock />
