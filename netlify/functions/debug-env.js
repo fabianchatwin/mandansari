abc45286b03b71d5ef6bad838b4fd00ed40fd268
Fabian Chatwin Cedrati <fabianchatwin@gmail.com>
2024-06-07 18:58:25 +0200

    test

--- netlify/functions/debug-env.js
+++ netlify/functions/debug-env.js
@@ -1,10 +1,12 @@
-// pages/api/debug-env.js
+// ./netlify/functions/debug-env.js
 
-export default function handler(req, res) {
-    res.status(200).json({
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
       NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
       NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
       CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET ? 'set' : 'not set',
-    });
-  }
-  
+    }),
+  };
+};
