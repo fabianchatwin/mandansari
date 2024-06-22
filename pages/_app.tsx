b0ece0cac6a2eb0ed05d482a604bb6eaa6d40d74
Fabian Chatwin Cedrati <fabianchatwin@gmail.com>
2024-06-23 01:21:12 +0200

    og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,12 +22,12 @@ function Application({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to fafalala!</title>
+        <title>Welcome to fafalala Support Network!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:image" content="https://fafalala.org/ws-image-1.jpg" />
         <meta property="og:image:width" content="600" />
         <meta property="og:image:height" content="400" />
-        <meta name="description" content="The fafalala Support Network is the crowdfunding project started at Fabian and Lia's wedding." />
+        <meta name="description" content="The crowdfunding project started at Fabian and Lia's wedding." />
       </Head>
       <Component {...pageProps} />
     </>
