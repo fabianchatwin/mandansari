3f54197066ee4afffd01170350d5124a782177c8
Fabian Chatwin Cedrati <fabianchatwin@gmail.com>
2024-06-22 19:08:25 +0200

    install ga-4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,21 @@ import Head from "next/head";
 import "../styles/fafalala.css";
 import "../styles/gallery.css";
 import './fontawesome';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { initGA, logPageView } from '../utils/ga';
 
 function Application({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    initGA();
+    logPageView();
+    router.events.on('routeChangeComplete', logPageView);
+    return () => {
+      router.events.off('routeChangeComplete', logPageView);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
---
+++ utils/ga.js
@@ -0,0 +1,10 @@
+// utils/ga.js
+import ReactGA from 'react-ga';
+
+export const initGA = () => {
+  ReactGA.initialize('G-2W6RTZ05CJ', { gaOptions: { siteSpeedSampleRate: 100 } });
+};
+
+export const logPageView = () => {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+};
