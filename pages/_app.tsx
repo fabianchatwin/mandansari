a0d12190518c867cb7ca4d4c0eb6a445791ec99f
Fabian Chatwin Cedrati <fabianchatwin@gmail.com>
2024-06-23 00:50:49 +0200

    og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function Application({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="https://media.fafalala.org/lot_people-15-small.jpg" />
+        <meta property="og:image" content="https://fafalala.org/lot_people-15-small.jpg" />
         <meta property="og:image:width" content="1024" />
         <meta property="og:image:height" content="683" />
         <meta property="og:image:alt" content="Welcome to fafalala.org" />        
